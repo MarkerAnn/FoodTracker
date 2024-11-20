e21c40f3dbe3bbc2abe498096b80ca49f1825b1b
Angelica Marker <am225cs@student.lnu.se>
2024-11-20 18:47:34 +0100

    Everything is working with test:coverage. I'm using Istanbul with Vitest, thats it

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true, // Active global variables like 'describe', 'test', 'it', 'expect'.
+    globals: true, // Activate global variables like 'describe', 'test', 'it', 'expect'.
     environment: 'node', // Choose 'node' or 'browser'.
     coverage: {
-      provider: 'istanbul', // Chose 'v8' or 'istanbul'.
+      provider: 'istanbul', // Choose 'v8' or 'istanbul'.
       reporter: ['text', 'html'], // Generate reports in text and html format.
       all: true, // Coverage for all files.
       include: ['src/**/*.ts'], // Which files to include.
-      exclude: ['tests/**', 'dist/**'], // Which files to exclude.
+      exclude: ['tests/**', 'dist/**', 'vitest.config.ts'], // Which files to exclude.
       reportsDirectory: './coverage', // Directory to save the reports.
     },
   },

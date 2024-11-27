0ede5af96f5dc69e91766d1d05e4726e8285e583
Angelica Marker <am225cs@student.lnu.se>
2024-11-27 15:55:33 +0100

    GREEN

--- src/validation/RecipeValidator.ts
+++ src/validation/RecipeValidator.ts
@@ -1,3 +1,9 @@
 import { Recipe } from '../models/Recipe'
 
-export class RecipeValidator {}
+export class RecipeValidator {
+  static validate(recipe: Recipe) {
+    if (!recipe || typeof recipe !== 'object') {
+      throw new Error('Recipe must be a valid object.')
+    }
+  }
+}

8df6dc1eeb5a00e825e50310b660d01dd521c164
Angelica Marker <am225cs@student.lnu.se>
2024-11-27 15:58:57 +0100

    GREEN

--- src/validation/RecipeValidator.ts
+++ src/validation/RecipeValidator.ts
@@ -5,5 +5,11 @@ export class RecipeValidator {
     if (!recipe || typeof recipe !== 'object') {
       throw new Error('Recipe must be a valid object.')
     }
+
+    if (!recipe.name || typeof recipe.name !== 'string') {
+      throw new Error(
+        'Recipe must have a valid name as a non-empty string.',
+      )
+    }
   }
 }

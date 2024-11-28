7563dbb25d03d4374500ed4afe33a31ea763755c
Angelica Marker <am225cs@student.lnu.se>
2024-11-28 04:29:37 +0100

    GREEN

--- src/validation/RecipeValidator.ts
+++ src/validation/RecipeValidator.ts
@@ -1,29 +1,34 @@
-// import { Recipe } from '../models/Recipe'
+import { Recipe } from '../models/Recipe'
 
-// export class RecipeValidator {
-//   static validate(recipe: Recipe) {
-//     if (!recipe || typeof recipe !== 'object') {
-//       throw new Error('Recipe must be a valid object.')
-//     }
+export class RecipeValidator {
+  ValidateRecipe(recipe: Recipe): void {
+    if (!recipe || typeof recipe !== 'object') {
+      throw new Error('Recipe must be a valid object.')
+    }
+  }
+  //   static validate(recipe: Recipe) {
+  //     if (!recipe || typeof recipe !== 'object') {
+  //       throw new Error('Recipe must be a valid object.')
+  //     }
 
-//     if (!recipe.name || typeof recipe.name !== 'string') {
-//       throw new Error('Recipe must have a valid name as a non-empty string.')
-//     }
+  //     if (!recipe.name || typeof recipe.name !== 'string') {
+  //       throw new Error('Recipe must have a valid name as a non-empty string.')
+  //     }
 
-//     if (typeof recipe.servings !== 'number' || recipe.servings <= 0) {
-//       throw new Error(
-//         'Recipe must have a positive amount of servings greater than 0.',
-//       )
-//     }
+  //     if (typeof recipe.servings !== 'number' || recipe.servings <= 0) {
+  //       throw new Error(
+  //         'Recipe must have a positive amount of servings greater than 0.',
+  //       )
+  //     }
 
-//     if (!recipe.instructions || typeof recipe.instructions !== 'string') {
-//       throw new Error(
-//         'Recipe must have a valid instructions as a non-empty string.',
-//       )
-//     }
+  //     if (!recipe.instructions || typeof recipe.instructions !== 'string') {
+  //       throw new Error(
+  //         'Recipe must have a valid instructions as a non-empty string.',
+  //       )
+  //     }
 
-//     if (!recipe.ingredients || !recipe.ingredients.length) {
-//       throw new Error('Recipe must have at least one ingredient.')
-//     }
-//   }
-// }
+  //     if (!recipe.ingredients || !recipe.ingredients.length) {
+  //       throw new Error('Recipe must have at least one ingredient.')
+  //     }
+  //   }
+}

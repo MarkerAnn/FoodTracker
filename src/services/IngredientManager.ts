27ec8e65881254e0c0dd0cb0851a1bfd99d06198
Angelica Marker <am225cs@student.lnu.se>
2024-11-28 01:52:07 +0100

    REFACTOR

--- src/services/IngredientManager.ts
+++ src/services/IngredientManager.ts
@@ -21,29 +21,19 @@ export class IngredientManager {
     return this.ingredients
   }
 
-  deleteIngredient(id: string): void {
-    this.findIngredient(id)
-    if (!this.findIngredient(id)) {
+  deleteIngredient(id: string) {
+    if (this.findIngredient(id)) {
+      this.ingredients = this.ingredients.filter(
+        (ingredient) => ingredient.id !== id,
+      )
+    } else {
       throw new Error('Ingredient does not exist.')
     }
-    this.ingredients = this.ingredients.filter(
-      (ingredient) => ingredient.id !== id,
-    )
   }
 
   private findIngredient(id: string) {
     return this.ingredients.find((ingredient) => ingredient.id === id)
   }
-
-  // deleteIngredient(id: string) {
-  //   if (this.findIngredient(id)) {
-  //     this.ingredients = this.ingredients.filter(
-  //       (ingredient) => ingredient.id !== id,
-  //     )
-  //   } else {
-  //     throw new Error('Ingredient does not exist.')
-  //   }
-  // }
 
   // private findIngredient(id: string) {
   //   return this.ingredients.find((ingredient) => ingredient.id === id)

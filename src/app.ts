6aa4c4f99ff624aba61230715da34733901cd9cd
Angelica Marker <am225cs@student.lnu.se>
2024-12-11 12:21:47 +0100

    REFACTOR: deleted outcomment code in app.ts, changed a script in package.json

--- src/app.ts
+++
@@ -1,5 +0,0 @@
-// import { main } from './views/consoleApp'
-// import { IngredientManager } from './services/IngredientManager'
-
-// const ingredientManager = new IngredientManager()
-// main(ingredientManager)
